85f87b01f8e8a58de98201f732f175f98961a48d
ashirbad behera <ashirbadbehera29@gmail.com>
2020-08-30 19:26:59 +0530

    added the generate new array button

--- src/SortingVisualizer/Visualizer.js
+++ src/SortingVisualizer/Visualizer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './SortingVisualizer.css';
 
 const Visualizer = () => {
+	// state of the array
 	const [arr, setArr] = useState([]);
 
 	// generate random numberes
@@ -9,31 +10,36 @@ const Visualizer = () => {
 		return Math.floor(Math.random() * (500 - 10) + 10);
 	};
 
-	// size 30
 	const populateArray = () => {
 		const tempArr = [];
 		for (let i = 0; i < 160; i++) tempArr.push(generateRandomNumber());
 		return tempArr;
 	};
 
+	// before the app renders we have to populate the array
 	useEffect(() => {
 		setArr(populateArray());
 	}, []);
 
 	return (
-		<div className="visualizeContainer">
-			{arr.map((item, index) => {
-				return (
-					<div
-						className="arrayBar"
-						style={{
-							height: `${item}px`,
-						}}
-						key={index}
-					></div>
-				);
-			})}
-		</div>
+		<>
+			<div className="visualizeContainer">
+				{arr.map((item, index) => {
+					return (
+						<div
+							className="arrayBar"
+							style={{
+								height: `${item}px`,
+							}}
+							key={index}
+						></div>
+					);
+				})}
+			</div>
+			<button onClick={() => setArr(populateArray())} className="button">
+				New Array
+			</button>
+		</>
 	);
 };
 
