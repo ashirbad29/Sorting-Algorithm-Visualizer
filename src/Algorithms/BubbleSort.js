c1f607b9443ef38a1094927d2a461d52c5f8e1c7
ashirbad behera <ashirbadbehera29@gmail.com>
2020-09-02 22:46:58 +0530

    Formatted the code a little

---
+++ src/Algorithms/BubbleSort.js
@@ -0,0 +1,12 @@
+export const BubbleSort = arr => {
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return arr;
+};
--- src/SortingVisualizer/Visualizer.js
+++ src/SortingVisualizer/Visualizer.js
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import './SortingVisualizer.css';
+import { BubbleSort } from '../Algorithms/BubbleSort';
+
+// CONSTANTS
+const PRIMARY_COLOR = '#25CCF7';
+const ACTIVE_COLOR = '#EA425C';
+const SORTED_COLOR = '#45CE30';
+const ARRAY_LENGTH = 160;
 
 const Visualizer = () => {
 	// state of the array
 	const [arr, setArr] = useState([]);
 
-	// generate random numberes
+	// Random Number Genrator
 	const generateRandomNumber = () => {
-		return Math.floor(Math.random() * (500 - 10) + 10);
+		return Math.floor(Math.random() * (400 - 10) + 10);
 	};
 
+	// Populate The Array With Random Numbers
 	const populateArray = () => {
 		const tempArr = [];
-		for (let i = 0; i < 160; i++) tempArr.push(generateRandomNumber());
+		for (let i = 0; i < ARRAY_LENGTH; i++) tempArr.push(generateRandomNumber());
 		return tempArr;
 	};
 
-	// before the app renders we have to populate the array
+	// Render the Array Before DOM loades
 	useEffect(() => {
 		setArr(populateArray());
 	}, []);
+
+	/* Bubble Sort */
+	const bubbleSort = () => {
+		const animations = BubbleSort(arr);
+
+		const arrayBars = document.getElementsByClassName('arrayBar');
+		arrayBars[0].style.backgroundColor = ACTIVE_COLOR;
+	};
+
+	// IGNORE: utility function
+	function arraysAreSorted(a, b) {
+		if (a.length !== b.length) return false;
+		for (let i = 0; i < a.length; i++) {
+			if (a[i] !== b[i]) return false;
+		}
+		return true;
+	}
 
 	return (
 		<>
@@ -30,6 +55,7 @@ const Visualizer = () => {
 							className="arrayBar"
 							style={{
 								height: `${item}px`,
+								backgroundColor: PRIMARY_COLOR,
 							}}
 							key={index}
 						></div>
@@ -38,6 +64,9 @@ const Visualizer = () => {
 			</div>
 			<button onClick={() => setArr(populateArray())} className="button">
 				New Array
+			</button>
+			<button className="button" onClick={() => bubbleSort()}>
+				BubbleSort
 			</button>
 		</>
 	);
