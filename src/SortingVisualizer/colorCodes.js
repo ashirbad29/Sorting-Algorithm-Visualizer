46d5c59056c12a985672faaae8c4487dd527cd60
ashirbad behera <ashirbadbehera29@gmail.com>
2021-02-09 20:25:43 +0530

    ❄all colors codes in separete file

---
+++ src/SortingVisualizer/colorCodes.js
@@ -0,0 +1,10 @@
+const colors = {
+	primaryColor: '#dd85e7',
+	afterSortingColor: '#00587a',
+	pivotActiveColor: '#ff2400',
+	sortedElementColor: '#4cbb17',
+	cyan: '#40E0D0',
+	orange: '#FFA500',
+};
+
+export default colors;
